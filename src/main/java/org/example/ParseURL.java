4bd5b1bad3489c9da0c8a737cf3c2ab6acd71982
leary <leary1@uandex,ru>
2023-03-12 19:39:26 +0600

    СХС

--- src/main/java/org/example/ParseURL.java
+++ src/main/java/org/example/ParseURL.java
@@ -7,17 +7,15 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class ParseURL {
-    public static Document getSiteCode(String site){
+    public static Document getPageCode(String site) {
         try {
             Document doc = Jsoup.connect(site)
                     .userAgent("Chrome/4.0.249.0 Safari/532.5")
                     .referrer("http://www.google.com")
                     .get();
 
-            Elements aba = doc.select("iframe");
-
             return doc;
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
