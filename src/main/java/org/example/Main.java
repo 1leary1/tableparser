9730dc91157d1b8d51ec4052e941b46eff6f9835
nondescript <you@example.com>
2023-03-12 21:21:33 +0600

    regex

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Main {
     public static String standardUrl = "https://onedrive.live.com/redir?resid={resid}&authkey={authkey}&page=Download";
@@ -12,5 +15,18 @@ public class Main {
     public static void main(String[] args) {
         Document page = ParseURL.getPageCode(site);
         Elements elements = page.select("iframe");
+        System.out.println(elements);
+
+        String resid = "";
+        String authkey = "";
+
+        Matcher residMatcher = Pattern.compile("resid=\\S*?&amp").matcher(String.valueOf(elements));
+        if (residMatcher.find()) resid = residMatcher.group().replace("resid=","");
+
+        Matcher authMatcher = Pattern.compile("authkey=\\S*?&amp").matcher(String.valueOf(elements));
+        if (authMatcher.find()) authkey = authMatcher.group().replace("authkey=","");
+
+        System.out.println("authkey бля = "+authkey+"\n"+ "resid нах = "+ resid);
+
     }
 }
